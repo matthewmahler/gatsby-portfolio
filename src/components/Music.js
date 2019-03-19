f1d779c970feb0c12a7af65fe5974ba53e8ae7a9
matthewmahler <matthewjamesmahler@gmail.com>
2019-03-19 10:26:58 -0400

    mobile bug fix

--- src/components/Music.js
+++ src/components/Music.js
@@ -1,15 +1,4 @@
 import React from "react";
-import Slider from "react-slick";
-
-const settings = {
-  // dots: true,
-  infinite: true,
-  autoplay: false,
-  speed: 500,
-  slideToShow: 1,
-  slideToScroll: 1,
-  swipeToSlide: true
-};
 
 const Music = () => (
   <section id="music">
@@ -17,30 +6,28 @@ const Music = () => (
       <div className="row">
         <div className="ten columns">
           <h1>My Music</h1>
-          <ul>
-            <Slider {...settings}>
-              <li>
-                <iframe
-                  title="Feeny"
-                  src="https://open.spotify.com/embed/artist/4TWQJppHQYlY4FlzuvEDUc"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
-                  allowTransparency="true"
-                />
-              </li>
-              <li>
-                <iframe
-                  title="Guilty Spark"
-                  src="https://open.spotify.com/embed/artist/6a0A3HMwpFmUJwZ39VfgO5"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
-                  allowTransparency="true"
-                />
-              </li>
-            </Slider>
-          </ul>
+          <div className="caro-container">
+            <div>
+              <iframe
+                title="Feeny"
+                src="https://open.spotify.com/embed/artist/4TWQJppHQYlY4FlzuvEDUc"
+                width="100%"
+                height="400"
+                frameBorder="0"
+                allowTransparency="true"
+              />
+            </div>
+            <div>
+              <iframe
+                title="Guilty Spark"
+                src="https://open.spotify.com/embed/artist/6a0A3HMwpFmUJwZ39VfgO5"
+                width="100%"
+                height="400"
+                frameBorder="0"
+                allowTransparency="true"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
